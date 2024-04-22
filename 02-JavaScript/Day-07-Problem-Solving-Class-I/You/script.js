83c922010d0ca88b2885865b1408d858e84e7f3a
Nageshwar <nageshpawarpatil@gmail.com>
2024-04-22 23:33:08 +0530

    Problem Solving Class I Done

--- 02-JavaScript/Day-07-Problem-Solving-Class-I/You/script.js
+++ 02-JavaScript/Day-07-Problem-Solving-Class-I/You/script.js
@@ -1,3 +1,5 @@
+// Products array containing product data
+
 let Products = [
   {
     title: "Mobile",
@@ -7,7 +9,7 @@ let Products = [
   },
   {
     title: "Mobile",
-    description: "16 GB RAM | 64 MP Camera",
+    description: "12 GB RAM | 64 MP Camera",
     img: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/g/d/1/-original-imagz6tzjgggzygu.jpeg?q=70",
     price: 47999,
   },
@@ -49,20 +51,21 @@ let Products = [
   },
 ];
 
-// console.log(Products);
-
-Products = Products.map((product, index) => {
-  return { ...product, id: index + 1 };
-});
-
-// console.log(Products);
-
+// Get reference to the products container
 let products_container = document.getElementById("products_container");
 
+// Get references to DOM elements
+let searchBar = document.querySelector("#navbar > input");
+let searchBtn = document.getElementById("navbar_search");
+let select = document.querySelector("select");
+let form = document.querySelector("form");
+
+// Function to display products in the products container
 function showData(Products) {
   products_container.innerHTML = null;
 
-  Products.forEach((product) => {
+  // Iterate over each product and create HTML elements to display them
+  Products.forEach((product, index) => {
     let div = document.createElement("div");
     div.className = "product";
 
@@ -78,10 +81,80 @@ function showData(Products) {
     let price = document.createElement("p");
     price.innerHTML = `Price : <strong>${product.price}</strong>`;
 
-    div.append(img, title, description, price);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
 
+    // Add event listener to delete button to remove corresponding product
+    deleteBtn.addEventListener("click", () => {
+      deleteData(index);
+    });
+
+    div.append(img, title, description, price, deleteBtn);
     products_container.append(div);
   });
 }
 
+// Function to filter products based on search input
+function searchData() {
+  let newData = Products.filter((el) => {
+    return el.title.toLocaleLowerCase() === searchBar.value.toLocaleLowerCase();
+  });
+
+  // Display filtered products or "Product Not Found" message
+  if (newData.length > 0) {
+    showData(newData);
+  } else {
+    products_container.innerHTML = `<h1>Product Not Found</h1>`;
+  }
+}
+
+// Function to sort products based on selected option
+function sortData(arr, val) {
+  if (val === "low_to_high") {
+    let newArr = arr.sort((a, b) => a.price - b.price);
+    showData(newArr);
+  } else if (val === "high_to_low") {
+    let newArr = arr.sort((a, b) => b.price - a.price);
+    showData(newArr);
+  }
+}
+
+// Function to delete a product
+function deleteData(index) {
+  Products.splice(index, 1);
+  showData(Products);
+}
+
+// Function to handle form submission (adding new product)
+function handleSubmit(event) {
+  let product = {};
+  event.preventDefault();
+  let inputArr = [...event.target];
+
+  // Create a new product object from form input values
+  product.title = inputArr[0].value;
+  product.description = inputArr[1].value;
+  product.img = inputArr[2].value;
+  product.price = +inputArr[3].value;
+
+  // Add the new product to the Products array and display updated products
+  Products.push(product);
+  showData(Products);
+  console.log(Products);
+}
+
+// Event listeners
+select.addEventListener("change", function () {
+  if (select.value) {
+    sortData(Products, select.value);
+  }
+});
+
+searchBtn.addEventListener("click", searchData);
+
+form.addEventListener("submit", function (event) {
+  handleSubmit(event);
+});
+
+// Initial display of products
 showData(Products);
