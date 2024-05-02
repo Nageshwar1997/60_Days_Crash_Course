de98fe631147696f044c508ae12540ce9511f5e7
Nageshwar <nageshpawarpatil@gmail.com>
2024-05-03 01:32:11 +0530

    Immediately Invoked Function Expression Added

---
+++ 02-JavaScript/Day-13-Problem-Solving-Class-III/IIFE.js
@@ -0,0 +1,16 @@
+// IIFE ---> Immediately Invoked Function Expression
+
+// 1. Normal Function
+(function greet() {
+  console.log("Hello World");
+})();
+
+// 2. Anonymous Arrow function
+(() => {
+  console.log("Hello World");
+})();
+
+// 3. Anonymous Function
+(function () {
+  console.log("Hello World");
+})();
