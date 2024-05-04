a95bd385f817eb97a833888fa1d2934416b15e37
Nageshwar <nageshpawarpatil@gmail.com>
2024-05-04 22:03:49 +0530

    Modified

--- 02-JavaScript/Day-12-Fetch-POST_PUT_PATCH_DELETE-Methods_And_ES6-Import-Export/Assignment/JSON-Server-E-Commerce-App/script.js
+++ 02-JavaScript/Day-12-Fetch-POST_PUT_PATCH_DELETE-Methods_And_ES6-Import-Export/Assignment/JSON-Server-E-Commerce-App/script.js
@@ -1,5 +1,6 @@
 let root = document.getElementById("root");
-let baseUrl = "http://localhost:3000";
+// let baseUrl = "http://localhost:3000";
+let baseUrl = "https://fakestoreapi.com";
 
 fetchData(`${baseUrl}/products`);
 
@@ -7,7 +8,7 @@ async function fetchData(URL) {
   try {
     let resp = await fetch(URL);
     let data = await resp.json();
-    console.log(data);
+    // console.log(data);
     showData(data);
   } catch (error) {
     console.log(error);
@@ -20,28 +21,68 @@ function showData(arr) {
     let card = document.createElement("div");
     card.className = "card";
 
-    card.innerHTML = `
-    <div class="imageDiv">
-        <img src=${product.src} alt=${product.title} />
-    </div>
-    <p>Title: <strong>${product.title}</strong></p>
-    <p>Price: <strong>₹${product.price}</strong></p>
-    <p>Ratings: <strong>${product.ratings} ⭐</strong></p>
-    <div class="btnDiv">
-        <button onClick="addToCart(${index})">Add to Cart</button>
-        <button onClick="deleteFromCart(${index})">Delete from Cart</button>
-    </div>
-`;
+    //     card.innerHTML = `
+    //     <div class="imageDiv">
+    //         <img src=${product.image} alt=${product.title} />
+    //     </div>
+    //     <p>Title: <strong>${product.title}</strong></p>
+    //     <p>Category: <strong>${product.category}</strong></p>
+    //     <p>Price: <strong>$${product.price}</strong></p>
+    //     <p>Ratings: <strong>${product.rating.rate} ⭐</strong></p>
+    //     <div class="btnDiv">
+    //         <button onClick="addToCart(${index})">Add to Cart</button>
+    //         <button onClick="deleteFromCart(${index})">Delete from Cart</button>
+    //     </div>
+    // `;
 
+    let imageDiv = document.createElement("div");
+    imageDiv.className = "imageDiv";
+    let image = document.createElement("img");
+    image.src = product.image;
+    image.alt = product.title;
+
+    imageDiv.appendChild(image);
+
+    let title = document.createElement("p");
+    title.innerHTML = `Title: <strong>${product.title}</strong>`;
+
+    let category = document.createElement("p");
+    category.innerHTML = `Category: <strong>${product.category}</strong>`;
+
+    let price = document.createElement("p");
+    price.innerHTML = `Price: <strong>$${product.price}</strong>`;
+
+    let ratings = document.createElement("p");
+    ratings.innerHTML = `Ratings: <strong>${product.rating.rate} ⭐`;
+
+    let btnDiv = document.createElement("div");
+    btnDiv.className = "btnDiv";
+
+    let addToCartBtn = document.createElement("button");
+    addToCartBtn.textContent = "Add to Cart";
+
+    addToCartBtn.addEventListener("click", () => {
+      addToCart(product);
+    });
+
+    let removeFromCartBtn = document.createElement("button");
+    removeFromCartBtn.textContent = "Delete from Cart";
+
+    removeFromCartBtn.addEventListener("click", () => {
+      deleteFromCart(product);
+    });
+
+    btnDiv.append(addToCartBtn, removeFromCartBtn);
+    card.append(imageDiv, title, category, price, ratings, btnDiv);
     root.append(card);
   });
 }
-function addToCart(index) {
-  console.log(index);
-  alert("Added to Cart");
+function addToCart(product) {
+  console.log(product.title);
+  alert(product.title + " Added to Cart");
 }
 
-function deleteFromCart(index) {
-  console.log(index);
-  alert("Deleted from Cart");
+function deleteFromCart(product) {
+  console.log(product.title);
+  alert(product.title + " Deleted from Cart");
 }
