375a7306d41d4812c32c552f66af5b3cccca40ec
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-21 18:21:19 +0530

    Changes Made

--- 04-DSA/00-Masterclass/01-Complexity-Analysis/Task01.js
+++ 04-DSA/00-Masterclass/01-Complexity-Analysis/Task01.js
@@ -2,10 +2,10 @@
 
 let length = 10;
 let width = 20;
+const A = area(length, width);
+const P = perimeter(length, width);
 
-console.log(area(length, width));
-
-console.log(perimeter(length, width));
+console.log(`Area : ${A} | Perimeter : ${P}`);
 
 function area(length, width) {
   return length * width;
